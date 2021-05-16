b8f34b4028a3cc0ba7abc30b4b4b42f148ac1ad5
codelear <39409007+codelear@users.noreply.github.com>
2021-05-16 15:55:46 +0200

    animate cart item count

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,10 +2,22 @@ import { Nav, Navbar, Button, Badge } from "react-bootstrap";
 import ReactDOM from "react-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { UseCartContext } from "./Cart";
+import { useEffect, useState } from "react";
+import classes from "./NavBar.module.css";
 
 function NavBar(props) {
 
   const cartctx = UseCartContext();
+
+  const [isCartHighlighted, setIsCartHighlighted]=useState(false);
+
+  const highlightclass = `${isCartHighlighted ? classes.bump : ''}`;
+
+  useEffect(()=>{
+    setIsCartHighlighted(true);
+    const timer = setTimeout(()=>setIsCartHighlighted(false), 300);
+    return ()=>clearTimeout(timer);
+  }, [cartctx.items]);
 
   return ReactDOM.createPortal(
 
@@ -20,9 +32,9 @@ function NavBar(props) {
         </Nav>
       </Navbar.Collapse>
 
-      <Button variant="primary" onClick={cartctx.ShowCart}> 
+      <Button variant="primary" onClick={cartctx.ShowCart} > 
         <FontAwesomeIcon icon={"shopping-cart"} color="yellow"/>{" "}
-        <Badge variant="light">{cartctx.items.reduceRight((a,b)=>a+b.item_count, +0)}</Badge>
+        <Badge variant="light" className={highlightclass}>{cartctx.items.reduceRight((a,b)=>a+b.item_count, +0)}</Badge>
         <span className="sr-only">Items in Cart</span>
       </Button>
 
